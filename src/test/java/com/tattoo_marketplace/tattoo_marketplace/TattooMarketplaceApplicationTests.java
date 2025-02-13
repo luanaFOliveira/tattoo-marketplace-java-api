450bc27dcdc6ea9ce4c627164086bb0a2807eb2d
luanaFOliveira <luanaFOliveirabr@gmail.com>
2025-02-13 15:32:20 -0300

    inicialize project

---
+++ src/test/java/com/tattoo_marketplace/tattoo_marketplace/TattooMarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tattoo_marketplace.tattoo_marketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TattooMarketplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
