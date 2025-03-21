08b0d68c3f459cba6bbb7235fa1603f971417e38
luanaFOliveira <luanaFOliveirabr@gmail.com>
2025-03-21 16:15:25 -0300

    fix: changed get status and category routes autorization

--- src/main/java/com/tattoo_marketplace/infra/config/SecurityConfiguration.java
+++ src/main/java/com/tattoo_marketplace/infra/config/SecurityConfiguration.java
@@ -47,7 +47,7 @@ public class SecurityConfiguration {
                 authorize -> authorize
                         .requestMatchers(unauthenticatedEndpoints).permitAll()
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/tattoo-artist", "/tattoo-artist/{tattooArtistId}").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/tattoo-artist", "/tattoo-artist/{tattooArtistId}", "/images/**","/status","/category").permitAll()
                         .anyRequest().authenticated()
             )
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
